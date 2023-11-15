9a1bb7b3c44973cd8cd7f3a7bd0b6f80bf986f57
w0oirke <96587188+w0oirke@users.noreply.github.com>
2023-11-16 01:21:46 +0900

    sjf

--- main.py
+++ main.py
@@ -12,10 +12,7 @@ os.makedirs(folder_path, exist_ok=True)
 url = input("주소 입력 (input url): ")
 response = requests.get(url)
 if response.status_code == 200:
-    # HTML 파일 저장 경로
     file_path = os.path.join(folder_path, "index.html")
-
-    # HTML 내용 저장
     html = response.text
     with open(file_path, "w", encoding="utf-8") as f:
         f.write(html)
