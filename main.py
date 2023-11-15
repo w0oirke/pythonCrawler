6ec16bfefbc2b697b34d3e963ad8e085ffe14fdb
w0oirke <96587188+w0oirke@users.noreply.github.com>
2023-11-15 23:46:39 +0900

    fdsa

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ response = requests.get(url)
 
 if response.status_code == 200:
     html = response.text 
-    with open("webpage.html", "w", encoding="utf-8") as f:
+    with open("index.html", "w", encoding="utf-8") as f:
         f.write(html)
 else:
     print("error")
