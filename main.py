d82f6a3e5a26c14ae437aaecce3acbf117d67483
w0oirke <96587188+w0oirke@users.noreply.github.com>
2023-11-15 23:36:10 +0900

    fsad

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ url = input("주소 입력 (input url) : ")
 response = requests.get(url)
 
 if response.status_code == 200:
-    html = response.text  # 웹페이지의 HTML 내용
+    html = response.text 
     with open("webpage.html", "w", encoding="utf-8") as f:
         f.write(html)
 else:
