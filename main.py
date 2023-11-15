4858ba94c21094ec20744a4f9410d2f3de9244cc
w0oirke <96587188+w0oirke@users.noreply.github.com>
2023-11-16 01:20:54 +0900

    sjf

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import requests
+import os
 
-url = input("주소 입력 (input url) : ") 
+current_folder = os.getcwd()
 
+folder_name = 'html'
+
+folder_path = os.path.join(current_folder, folder_name)
+
+os.makedirs(folder_path, exist_ok=True)
+
+url = input("주소 입력 (input url): ")
 response = requests.get(url)
-
 if response.status_code == 200:
-    html = response.text 
-    with open("index.html", "w", encoding="utf-8") as f:
+    # HTML 파일 저장 경로
+    file_path = os.path.join(folder_path, "index.html")
+
+    # HTML 내용 저장
+    html = response.text
+    with open(file_path, "w", encoding="utf-8") as f:
         f.write(html)
 else:
     print("error")
